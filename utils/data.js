3b2af4701a532932d76da7d39a8c814167b1a87e
Leo Wong <leoelicos@gmail.com>
2022-06-26 20:58:33 +1000

    Remove comments

--- utils/data.js
+++ utils/data.js
@@ -4,13 +4,10 @@ const names = ['Aaran', 'Aaren', 'Aarez', 'Aarman', 'Aaron', 'Aaron-James', 'Aar
 
 const appDescriptions = ['Decision Tracker', 'Find My Phone', 'Learn Piano', 'Starbase Defender', 'Tower Defense', 'Monopoly Money Manager', 'Movie trailers', 'Hello world', 'Stupid Social Media App', 'Notes', 'Messages', 'Email', 'Compass', 'Firefox', 'Running app', 'Cooking app', 'Poker', 'Deliveries'];
 
-// Get a random item given an array
 const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
-// Gets a random full name
 const getRandomName = () => `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
 
-// Function to generate random reactions that we can add to student object.
 const getRandomReactions = (int) => {
   const results = [];
   for (let i = 0; i < int; i += 1) {
@@ -22,5 +19,4 @@ const getRandomReactions = (int) => {
   return results;
 };
 
-// Export the functions for use in seed.js
 module.exports = { getRandomName, getRandomReactions };
