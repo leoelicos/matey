66b3bdfdab151b4abd0370ae7d3b25cb6c836cf5
Leo Wong <leoelicos@gmail.com>
2022-06-26 20:56:22 +1000

    rename database

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 
 const { connect, connection } = require('mongoose');
 
-const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/studentsDB';
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/socialNetworkDB';
 
 connect(connectionString, {
   useNewUrlParser: true,
