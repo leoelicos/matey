c5307f21218bf5a95d8c67513d560e2e71d61891
Leo Wong <leoelicos@gmail.com>
2023-03-12 20:46:04 +1100

    add comments

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,17 @@
 
 const { connect, connection } = require('mongoose');
 
+// On Heroku, Node uses the environment mongoDB URI
+// Locally, Node uses the explicit mongoDB URI
 const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/socialNetworkDB';
 
+// Wrap mongoose around connection to MongoDB
 connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
+// export connection
 module.exports = connection;
+
+ 
