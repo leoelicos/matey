238ec3507ba9d554c1a7149f970577b563b12061
Leo Wong <leoelicos@gmail.com>
2023-03-14 04:48:03 +1100

    Convert to ES Module

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,15 @@
-'use strict';
-
-const { connect, connection } = require('mongoose');
+import { connect, connection } from 'mongoose'
 
 // On Heroku, Node uses the environment mongoDB URI
 // Locally, Node uses the explicit mongoDB URI
-const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/socialNetworkDB';
+// const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/socialNetworkDB'
+const connectionString = 'mongodb://localhost:27017/socialNetworkDB'
 
 // Wrap mongoose around connection to MongoDB
 connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-});
+})
 
 // export connection
-module.exports = connection;
-
- 
+export default connection
