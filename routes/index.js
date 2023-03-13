1155ece06327b24083268f27b874f14383d46166
Leo Wong <leoelicos@gmail.com>
2023-03-14 04:56:30 +1100

    ESM Import replacing require

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-const router = require('express').Router()
-import apiRoutes from './api'
+import Router from 'express'
+const router = Router()
+import apiRoutes from './api/index.js'
 
 router.use('/api', apiRoutes)
 
