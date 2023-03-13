9b5e30a88a0c12d1c05ac4c63da06bf380567fec
Leo Wong <leoelicos@gmail.com>
2023-03-14 04:56:22 +1100

    ESM Import replacing require

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,7 @@
-const router = require('express').Router()
-import userRoutes from './userRoutes'
-import thoughtRoutes from './thoughtRoutes'
+import Router from 'express'
+const router = Router()
+import userRoutes from './userRoutes.js'
+import thoughtRoutes from './thoughtRoutes.js'
 
 router.use('/users', userRoutes)
 router.use('/thoughts', thoughtRoutes)
