dc66ae276b3e8884bd9da6671953c21d9af2d31f
Leo Wong <leoelicos@gmail.com>
2023-03-14 05:07:36 +1100

    Refactor to make it more readable

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,17 +1,32 @@
 import Router from 'express'
 const router = Router()
-import { getThoughts, getSingleThought, createThought, updateThought, deleteThought, addReaction, removeReaction } from '../../controllers/thoughtController.js'
+import {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction //
+} from '../../controllers/thoughtController.js'
 
-// /api/thoughts
-router.route('/').get(getThoughts).post(createThought)
+router //
+  .route('/')
+  .get(getThoughts)
+  .post(createThought)
 
-// /api/thoughts/:thoughtId
-router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought)
+router //
+  .route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought)
 
-// /api/thoughts/:thoughtId/reactions
-router.route('/:thoughtId/reactions').post(addReaction)
+router //
+  .route('/:thoughtId/reactions')
+  .post(addReaction)
 
-// /api/thoughts/:thoughtId/reactions/:reactionId
-router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction)
+router //
+  .route('/:thoughtId/reactions/:reactionId')
+  .delete(removeReaction)
 
 export default router
