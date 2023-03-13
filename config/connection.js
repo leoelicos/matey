45139948de9e9f4bfc87e1e35a43c31672b79307
Leo Wong <leoelicos@gmail.com>
2023-03-14 04:56:03 +1100

    ESM Import replacing require

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,8 @@
-import { connect, connection } from 'mongoose'
-
+import pkg from 'mongoose'
+const { connect, connection } = pkg
 // On Heroku, Node uses the environment mongoDB URI
 // Locally, Node uses the explicit mongoDB URI
-// const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/socialNetworkDB'
-const connectionString = 'mongodb://localhost:27017/socialNetworkDB'
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetworkDB'
 
 // Wrap mongoose around connection to MongoDB
 connect(connectionString, {
